0bde7da994b8fa6e7741546aa60cb712572e4970
Lerato-leo <leratomatamela1@gmail.com>
2023-07-20 20:06:17 +0200

    3

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,5 +1,5 @@
-def numbers():
-    for i in range(99):
-        return f"{i} = {hex(i)}"
+def print_numbers():
+    for num in range(99):
+        print("f{i} = {hex(i)}".format(num, num))
 
-print(numbers())
+print_numbers()
